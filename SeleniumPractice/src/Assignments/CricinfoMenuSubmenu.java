f005a4cd17043e8e39aea4ebc5e060709cc8020b
JavaGit-Automation <prk900@gmail.com>
2022-05-04 11:05:28 +0530

    Maven Programs.

--- SeleniumPractice/src/Assignments/CricinfoMenuSubmenu.java
+++ SeleniumPractice/src/Assignments/CricinfoMenuSubmenu.java
@@ -1,56 +1,49 @@
-package Assignments;
+package assignments;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import utilities.SeleniumUtility;
 
-public class CricinfoMenuSubmenu {
+public class CricinfoMenuSubMenu {
 
 	public static void main(String[] args) throws InterruptedException {
-
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://www.espncricinfo.com/");
-		
+		SeleniumUtility s1=new SeleniumUtility();
+		WebDriver driver=s1.setUp("chrome", "https://www.espncricinfo.com/");
 		Actions act=new Actions(driver);
-		List<WebElement> allOptions=driver.findElements(By.xpath("//div[@class='ds-flex ds-flex-row']/div/a"));
-		for(int i=0;i<allOptions.size();i++) {
+		List<WebElement> mainMenu=driver.findElements(By.cssSelector(".ds-bg-fill-navbar .ds-flex-row>div.ds-popper-wrapper>a"));
+		for(int i=0;i<mainMenu.size();i++) {
 			try {
 				driver.findElement(By.xpath("//button[text()='Not Now']")).click();
-			}catch(NoSuchElementException e){
+			}catch(NoSuchElementException e) {
 				
 			}
-			System.out.println("*****"+allOptions.get(i).getText()+"*****");
-			act.moveToElement(allOptions.get(i)).perform();
+			System.out.println("***************"+mainMenu.get(i).getText()+"**************");
+			act.moveToElement(mainMenu.get(i)).perform();
 			Thread.sleep(1000);
-			List<WebElement> subOptions=driver.findElements(By.xpath("//ul[contains(@class,'ds-overflow-ellipsis')]/li/a/span"));
-			if(subOptions.size()>0) {
-				for(int j=0;j<subOptions.size()	;j++) {
-					subOptions=driver.findElements(By.xpath("//ul[contains(@class,'ds-overflow-ellipsis')]/li/a/span"));
+			List<WebElement> subMenu=driver.findElements(By.xpath("//ul[contains(@class,'ds-overflow-ellipsis')]/li/a/span"));
+			if(subMenu.size()>0) {
+				for(int j=0;j<subMenu.size();j++) {
+					subMenu=driver.findElements(By.xpath("//ul[contains(@class,'ds-overflow-ellipsis')]/li/a/span"));
 					try {
-						System.out.println("*****"+subOptions.get(j).getText());
+						System.out.println(subMenu.get(j).getText());
 					}catch(StaleElementReferenceException e) {
 						driver.navigate().refresh();
-						act.moveToElement(allOptions.get(i)).perform();
+						act.moveToElement(mainMenu.get(i)).perform();
 						Thread.sleep(1000);
-						subOptions=driver.findElements(By.xpath("//ul[contains(@class,'ds-overflow-ellipsis')]/li/a/span"));
-						System.out.println(subOptions.get(j).getText());
+						subMenu=driver.findElements(By.xpath("//ul[contains(@class,'ds-overflow-ellipsis')]/li/a/span"));
+						System.out.println(subMenu.get(j).getText());
 					}
 				}
 			}
 		}
-
 	}
 
 }
